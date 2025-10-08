36c242eafdc7297f8aca3ba32af860f6465cbcef
U-BSD\456523 <456523@SHS-J5W2NW3.beaverton.k12.or.us>
2025-10-08 15:38:01 -0700

    Added welcome message.

---
+++ main.cpp
@@ -0,0 +1,10 @@
+/* Tomas Carranza Echaniz
+*  This program is going to be the most peak Zuul that has ever zuuled
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Welcome to BURGER QUEST 2: ELECTRIC BOOGALOO\nYou're going on a quest to get a BURGER.";
+}
